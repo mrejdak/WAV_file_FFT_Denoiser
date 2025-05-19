c890d9506f400d4f029824dfd8dbc1332819a9ee
Massny <pawel.fornagiel2@gmail.com>
2025-05-20 01:52:22 +0200

    docs: add comments for WAV file structure

--- src/models/WavFormat.rs
+++ src/models/WavFormat.rs
@@ -115,6 +115,19 @@ impl WavFile {
 
   // DETACHED METHODS
 
+// Offset  Size  Name             Description
+// 0         4   ChunkID          Contains the letters "RIFF" in ASCII form
+//                                (0x52494646 big-endian form).
+// 4         4   ChunkSize        36 + SubChunk2Size, or more precisely:
+//                                4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
+//                                This is the size of the rest of the chunk 
+//                                following this number.  This is the size of the 
+//                                entire file in bytes minus 8 bytes for the
+//                                two fields not included in this count:
+//                                ChunkID and ChunkSize.
+// 8         4   Format           Contains the letters "WAVE"
+//                                (0x57415645 big-endian form).
+
   pub fn new_head(chunk_size: u32) -> WavHead {
     WavHead {
       chunk_id: *b"RIFF",
@@ -122,6 +135,23 @@ impl WavFile {
       format: *b"WAVE",
     }
   }
+
+// Offset  Size  Name             Description
+// 12        4   Subchunk1ID      Contains the letters "fmt "
+//                                (0x666d7420 big-endian form).
+// 16        4   Subchunk1Size    16 for PCM.  This is the size of the
+//                                rest of the Subchunk which follows this number.
+// 20        2   AudioFormat      PCM = 1 (i.e. Linear quantization)
+//                                Values other than 1 indicate some 
+//                                form of compression.
+// 22        2   NumChannels      Mono = 1, Stereo = 2, etc.
+// 24        4   SampleRate       8000, 44100, etc.
+// 28        4   ByteRate         == SampleRate * NumChannels * BitsPerSample/8
+// 32        2   BlockAlign       == NumChannels * BitsPerSample/8
+//                                The number of bytes for one sample including
+//                                all channels. I wonder what happens when
+//                                this number isn't an integer?
+// 34        2   BitsPerSample    8 bits = 8, 16 bits = 16, etc.
 
   pub fn new_fmt(num_channels: u16, sample_rate: u32, bits_per_sample: u16) -> WavFmt {
     let audio_format = AudioFormat::Pcm;
@@ -140,6 +170,16 @@ impl WavFile {
       bits_per_sample,
     }
   }
+
+// Offset  Size  Name             Description
+// 36        4   Subchunk2ID      Contains the letters "data"
+//                                (0x64617461 big-endian form).
+// 40        4   Subchunk2Size    == NumSamples * NumChannels * BitsPerSample/8
+//                                This is the number of bytes in the data.
+//                                You can also think of this as the size
+//                                of the read of the subchunk following this 
+//                                number.
+// 44        *   Data             The actual sound data.
 
   pub fn new_data(subchunk_size: u32, data: AudioSamples) -> WavData {
     WavData {
@@ -211,11 +251,14 @@ impl WavFile {
     let data_subchunk = WavFile::find_chunk(data, b"data").ok_or(WavError::UnexpectedLength)?;
     let subchunk_size = data_subchunk.len() as u32 - 8;
     let audio_data = &data_subchunk[8..];
-    let data: AudioSamples = match (fmt.num_channels, fmt.bits_per_sample) {
+
+    let data_field: AudioSamples = match (fmt.num_channels, fmt.bits_per_sample) {
+      // 8 bits per sample
       (1, 8) => {
         let samples = audio_data.iter().map(|&b| b as i8).collect();
         AudioSamples::MonoI8(samples)
       }
+
       (2, 8) => {
         let samples = audio_data
           .chunks_exact(2)
@@ -226,6 +269,8 @@ impl WavFile {
           .collect();
         AudioSamples::StereoI8(samples)
       }
+      
+      // 16 bits per sample
       (1, 16) => {
         let samples = audio_data
           .chunks_exact(2)
@@ -233,6 +278,7 @@ impl WavFile {
           .collect();
         AudioSamples::MonoI16(samples)
       }
+
       (2, 16) => {
         let samples = audio_data
           .chunks_exact(4)
@@ -243,6 +289,8 @@ impl WavFile {
           .collect();
         AudioSamples::StereoI16(samples)
       }
+
+      // 32 bits per sample
       (1, 32) => {
         let samples = audio_data
           .chunks_exact(4)
@@ -250,6 +298,7 @@ impl WavFile {
           .collect();
         AudioSamples::MonoI32(samples)
       }
+
       (2, 32) => {
         let samples = audio_data
           .chunks_exact(8)
@@ -260,14 +309,12 @@ impl WavFile {
           .collect();
         AudioSamples::StereoI32(samples)
       }
+
+      // Unsupportex sample size
       _ => return Err(WavError::InvalidWAudioFormat),
     };
 
-    let wav_data = WavData {
-      subchunk_id: *b"data",
-      subchunk_size,
-      data,
-    };
+    let wav_data = WavFile::new_data(subchunk_size, data_field);
 
     Ok(wav_data)
   }
