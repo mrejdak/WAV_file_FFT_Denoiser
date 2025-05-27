2b61a26184fc2f265b01bfa6f78e21e83f0a68c7
Massny <pawel.fornagiel2@gmail.com>
2025-05-27 18:37:53 +0200

    feat: update main for testing, add example files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 mod models;
-use models::WavFormat::{self, *};
+use models::wav_file::*;
 
 fn main() {
-    let file_path = "file_example.wav";
-    let wav = WavFile::from_wav_file(file_path).unwrap();
-    println!("{}", wav.head);
-    println!("{}", wav.fmt);
-    println!("{}", wav.data);
+    let file_path = "noise_example.wav";
+    let mut wav = WavFile::from_wav_file(file_path).unwrap();
+    wav.denoise_data_fft(0.001);
     
-    }
+    wav.save_to_file("new_file.wav");
+  }
